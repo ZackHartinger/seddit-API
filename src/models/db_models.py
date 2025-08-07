326e37e906d28abe1ced220383eb8a04eec2f9c6
ZackHartinger <zackhrtngr@gmail.com>
2025-08-06 19:04:53 -0700

    Installed alembic for handling db migrations/model updates. Added vote total collumns to Comment and Post tables.

--- src/models/db_models.py
+++ src/models/db_models.py
@@ -17,6 +17,7 @@ class Post(SQLModel, table=True):
     created_at : datetime = Field(default_factory=datetime.now)
     title: str = Field
     description: str | None = Field(default=None)
+    post_vote_total: int | None = Field(default=None)
 
     user_id: int | None = Field(default=None, foreign_key="user.id")
     user: User | None = Relationship(back_populates="posts")
@@ -27,6 +28,7 @@ class Post(SQLModel, table=True):
 class Comment(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     text: str = Field
+    comment_vote_total: int | None = Field(default=None)
 
     reply_id: int | None = Field(default=None, foreign_key="comment.id")
     parent_comment: Optional["Comment"] = Relationship(back_populates="replies", sa_relationship_kwargs={"remote_side": "Comment.id"})
@@ -52,19 +54,3 @@ class Vote(SQLModel, table=True):
 
     comment_id: int | None = Field(default=None, foreign_key="comment.id")
     comment: Optional["Comment"] | None = Relationship(back_populates="comment_votes")
-
-
-
-
-
-# def create_owners_and_pets():
-#     with Session(engine) as session:
-#         zack = Person(name="Zack", age=31, secret_name="ZTOWN")
-#         session.add(zack)
-#         session.commit()
-
-#         theo = Pet(name="Theo", type="Cat", person_id=zack.id)
-#         session.add(theo)
-#         session.commit()
-
-#         print(zack.pets)
